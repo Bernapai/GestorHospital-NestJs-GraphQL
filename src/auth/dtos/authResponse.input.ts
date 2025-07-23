4214cc5da0e8f472a4b6be74944a8db59cb9bee0
Bernapai <juanemeunier@gmail.com>
2025-07-23 16:14:09 -0300

    Small Changes for Middlewares

--- src/auth/dtos/authResponse.input.ts
+++ src/auth/dtos/authResponse.input.ts
@@ -1,12 +1,12 @@
 // dto/auth.response.ts
 import { ObjectType, Field } from '@nestjs/graphql';
-import { Usuario } from 'src/users/entities/user.entity';
+import { registerInput } from './register.input';
 
 @ObjectType()
 export class AuthResponse {
     @Field()
     access_token: string;
 
-    @Field(() => Usuario)
-    usuario: Usuario;
+    @Field(() => registerInput)
+    usuario: registerInput;
 }
